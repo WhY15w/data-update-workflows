19f59130a0dbb95c6def84d8907bcfa029ba2d6f
Nattsu39 <nattsu39@outlook.com>
2026-03-02 16:12:53 +0800

    进一步支持强制更新功能

--- scripts/seer_unity_assets/update.py
+++ scripts/seer_unity_assets/update.py
@@ -1,9 +1,15 @@
 import asyncio
+import sys
 
 import albi0
 
-from scripts._common import DataRepoManager, get_current_time_str, write_to_github_output
+from scripts._common import (
+    DataRepoManager,
+    get_current_time_str,
+    write_to_github_output,
+)
 from scripts.seer_unity_assets.config import CONFIG
+
 
 def get_manifest_path(package_name: str) -> str:
     return f"package-manifests/{package_name}.json"
@@ -19,12 +25,14 @@ async def process_package(
     updater_name: str,
     update_args: list[str],
     extractor_name: str,
+    force: bool = False,
 ):
     async with albi0.session():
         await albi0.update_resources(
             updater_name,
             *update_args,
             manifest_path=get_manifest_path(package_name),
+            ignore_version=force,
         )
         await albi0.extract_assets(
             extractor_name,
@@ -33,10 +41,10 @@ async def process_package(
         )
 
 
-async def run():
+async def run(*, force: bool = False):
     albi0.load_all_plugins()
 
-    manager = DataRepoManager.from_checkout('.')
+    manager = DataRepoManager.from_checkout(".")
     for package_name, config in CONFIG.items():
         remote_version = await albi0.get_remote_version(config["updater_name"])
         print(f"⚙️ 正在更新资源包 {package_name}...")
@@ -45,11 +53,12 @@ async def run():
             updater_name=config["updater_name"],
             extractor_name=config["extractor_name"],
             update_args=config["update_args"],
+            force=force,
         )
         print(f"✅ 资源包 {package_name} 更新完成")
         manager.commit_and_push(
             f"{package_name}: Update to {remote_version} | Time: {get_current_time_str()}",
-            files=["package-manifests/", f"{config['extractor_name']}/assets/"]
+            files=["package-manifests/", f"{config['extractor_name']}/assets/"],
         )
 
     if manager.push():
@@ -57,4 +66,5 @@ async def run():
 
 
 def main():
-    asyncio.run(run())
+    force = "--force" in sys.argv
+    asyncio.run(run(force=force))
